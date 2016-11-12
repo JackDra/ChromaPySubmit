b205e354dbc1f10b964c15ba8a7e4a732b5f0295
Jack Dragos <jack.dragos@gmail.com>
2016-11-12 14:52:18 -0500

    started to add functionallity for PoF source shifts

--- FilenameFuns.py
+++ FilenameFuns.py
@@ -2,22 +2,30 @@
 
 from RunParams import *
 
-def Get2ptProp(icfg,ism):
-    return qpdir+CreateCfg(icfg)+'_tsrc'+str(it)+'_sm'+str(ism)+'.prop.lime'
+def ModuloTsrc(icfg,iPoF):
+    itsrc = SRCT[icfg]+(iPoF*PoFDelta)
+    # if itsrc < 0:
+    #     itsrc = itsrc+nt
+    if itsrc >= nt:
+        itsrc = itsrc-nt
+    return itsrc
 
-def Get2ptCorr(icfg,ism,jsm,interp):
-    return (cfdir+'twoptsm'+str(ism)+'si'+str(jsm)+'/'+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(it)+'sm'+str(ism)+'si'+str(jsm)+'_'+interp+'.2cf.lime')
+def Get2ptProp(icfg,ism,iPoF=0):
+    return qpdir+CreateCfg(icfg)+'_tsrc'+str(ModuloTsrc(icfg,iPoF))+'_sm'+str(ism)+'.prop.lime'
+
+def Get2ptCorr(icfg,ism,jsm,interp,iPoF=0):
+    return (cfdir+'twoptsm'+str(ism)+'si'+str(jsm)+'/'+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(ModuloTsrc(icfg,iPoF))+'sm'+str(ism)+'si'+str(jsm)+'_'+interp+'.2cf.lime')
 
 def Get2ptCorrFolders(icfg,ism,thisjsmlist):
     return [(cfdir+'twoptsm'+str(ism)+'si'+str(jsm)+'/') for jsm in thisjsmlist]
 
-def Get2ptCorrOutput(icfg,ism,jsm,interp):
-    return (cfdir+'/'+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(it)+'sm'+str(ism)+'si'+str(jsm)+'_'+interp+'.2cf.lime')
+def Get2ptCorrOutput(icfg,ism,jsm,interp,iPoF=0):
+    return (cfdir+'/'+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(ModuloTsrc(icfg,iPoF))+'sm'+str(ism)+'si'+str(jsm)+'_'+interp+'.2cf.lime')
 
 def Get3ptCorr(icfg,ism,tsink,Projector,DS,ND):
     thisREvecDir = (REvecFlag+'sm'+str(ism)+'_'+REvecFlag+'PoF'+str(PoFShifts)+'D'+str(PoFDelta)+
                     'GMA'+str(Projector)+'tsink'+str(tsink)+'p'+''.join(map(str,ppvec))+DS+'/')
-    return (cf3ptdir+thisREvecDir+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(it)+'sm'+str(ism)+'_'+REvecFlag+'PoF'+str(PoFShifts)+'D'+str(PoFDelta)+
+    return (cf3ptdir+thisREvecDir+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(ModuloTsrc(icfg,iPoF))+'sm'+str(ism)+'_'+REvecFlag+'PoF'+str(PoFShifts)+'D'+str(PoFDelta)+
             'GMA'+str(Projector)+'tsink'+str(tsink)+'p'+''.join(map(str,ppvec))+DS+ND+'.3cf.lime')
 
 def Get3ptCorrFolder(icfg,ism,tsink,Projector,DS):
@@ -28,7 +36,7 @@ def Get3ptCorrFolder(icfg,ism,tsink,Projector,DS):
 
 def Get3ptCorrjsm(icfg,ism,jsm,tsink,Projector,DS,ND):
     thisCMDir = 'CMsm'+str(ism)+'_si'+str(jsm)+'GMA'+str(Projector)+'tsink'+str(tsink)+'p'+''.join(map(str,ppvec))+DS+'/'
-    return (cf3ptdir+thisCMDir+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(it)+'sm'+str(ism)+'_si'+str(jsm)+'GMA'+str(Projector)+
+    return (cf3ptdir+thisCMDir+CreateCfg(icfg)+'_k'+str(kud)+'_tsrc'+str(ModuloTsrc(icfg,iPoF))+'sm'+str(ism)+'_si'+str(jsm)+'GMA'+str(Projector)+
             'tsink'+str(tsink)+'p'+''.join(map(str,ppvec))+DS+ND+'.3cf.lime')
 
 def Get3ptCorrFolderjsm(icfg,ism,jsm,tsink,Projector,DS):
