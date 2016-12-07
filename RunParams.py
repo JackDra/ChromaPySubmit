7313cde48289e3c3be6431db2688cf5afe0a93d1
Jack Dragos <jack.dragos@gmail.com>
2016-12-07 16:28:42 -0500

    Dont Run set true for testing on hpcc

--- RunParams.py
+++ RunParams.py
@@ -207,7 +207,7 @@ if len(codedir) == 0:
 ChromaFileFlag = 'params_runTwoPt_'
 
 ExitOnFail = True ## Reimplemented: exits if there was a failed run (on for debugging?)
-DontRun = False ## creates input files but does not run (for looking at .csh and .xml files
+DontRun = True ## creates input files but does not run (for looking at .csh and .xml files
 SaveMem = True ## saves memory in run by deleting sources and propagators when not needed.
 Save2ptProp = False ## Saves 2 point propagators for use in the 3 point correlator construction
 AveMom2pt = True ## Averages over all 2 point propagator momenta for a Q^2
