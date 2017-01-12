5b25b67f877f9f8eabd80f2f4b831c9423fa8751
Jack Dragos <jack.dragos@gmail.com>
2017-01-12 16:35:38 -0500

    something not quite right

--- CreateCfgFile.py
+++ CreateCfgFile.py
@@ -22,6 +22,7 @@ def CreateCfgList(ncfg,forcecfg,thisDupCfgs=DupCfgs):
         setfilelist = ['']
     else:
         filelist = os.listdir(gfdir)
+        
         # print filelist
         setfilelist = []
         if forcecfg == False:
@@ -29,12 +30,11 @@ def CreateCfgList(ncfg,forcecfg,thisDupCfgs=DupCfgs):
         else:
             if forcecfg[-1] == -1: forcecfg[-1] = len(filelist)
         for icf,ifile in enumerate(filelist):
-            if limename in ifile and icf+1 >= forcecfg[0] and icf+1 <= forcecfg[0]:
+            if (limename in ifile) and (icf+1 >= forcecfg[0]) and (icf+1 <= forcecfg[0]):
                 setfilelist.append(ifile.replace(limename,''))
             # if '.lime' in ifile:
                 # setfilelist.append('.'+'.'.join(ifile.split('.')[1:3]))
             # setfilelist.append(str(int(re.sub(r'.*lime','',ifile))))
-        print setfilelist
         setfilelist = SortConfigs(setfilelist)
         totncfg = len(setfilelist)
         # setfilelist = np.roll(setfilelist,machineroll*(totncfg/ncfg)/(totroll))
